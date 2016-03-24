f25a9ec3802fc3788884016cfec346f45f6972fc
Ronnie Nyaga <ronnienyaga@gmail.com>
2016-03-24 11:26:31 +0300

    Created task for gulp jshint and minify

--- gulpfile.js
+++ gulpfile.js
@@ -12,12 +12,26 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     del = require('del');
 
-    gulp.task('styles', function() {
-      return sass('src/styles/main.scss', { style: 'expanded' })
-        .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cssnano())
-        .pipe(gulp.dest('dist/assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
-    });
+//gulp-ruby-sass and autoprefixer
+gulp.task('styles', function() {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+//gulp-jshint, gulp-concat and gulp-uglify
+gulp.task('scripts', function() {
+return gulp.src('src/scripts/**/*.js')
+  .pipe(jshint('.jshintrc'))
+  .pipe(jshint.reporter('default'))
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest('dist/assets/js'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/assets/js'))
+  .pipe(notify({ message: 'Scripts task complete' }));
+});
